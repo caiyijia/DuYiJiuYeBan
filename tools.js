f3eb2b531aae1b2b1ee2a77c5a28f9c142952665
Adam Cai <caiyijias@outlook.com>
2019-06-21 16:01:17 +0800

    封装匀速运动startMove()

---
+++ tools.js
@@ -0,0 +1,63 @@
+// 兼容的事件绑定
+function addEvent(elem, type, handle) {
+    if (elem.addEventListener) {
+        elem.addEventListener(type, handle, false);
+    } else if (elem.attachEvent) {
+        elem.attachEvent('on' + type, function () {
+            // 此方法下this指向window
+            handle.call(elem);
+        })
+    } else {
+        elem['on' + type] = handle;
+    }
+}
+
+ // 阻止默认事件的函数
+ function cancelHandler(event) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }else {
+        event.returnValue = false;
+    }
+}
+
+//鼠标拖动事件
+function drag(elem) {
+    var disX,
+        disY;
+    addEvent(elem, 'mousedown', function(e) {
+        var event = e || window.event;
+        disX = event.clientX - parseInt(getStyle(elem, 'left'));
+        disY = event.clientY - parseInt(getStyle(elem, 'top'));
+        addEvent (document, 'mousemove', mouseMove);
+        addEvent(document, 'mouseup', mouseUp);
+        stopBubble(event);
+        cancelHandler(event);
+    });
+
+    function mouseMove(e) {
+        var event = e || window.event;
+        elem.style.left = event.clientX - disX + 'px';
+        elem.style.left = event.clientY - disY + 'px';
+    }
+
+    function mouseUp(e) {
+        var event = e || window.event;
+        removeEvent(document, 'mousemove', mouseMove);
+        removeEvent(document, 'mouseup', mouseUp);
+    }
+}
+
+//匀速运动
+function startMove(dom, velocity, target) {
+    clearInterval(timer);
+    var remain = target % velocity;
+    timer = setInterval(function () {
+        if (Math.abs(target - dom.offsetLeft) <= remain) {
+            clearInterval(timer);
+            dom.style.left = target + 'px';
+        } else {
+            dom.style.left = dom.offsetLeft + velocity + 'px';
+        }
+    }, 30);
+}
