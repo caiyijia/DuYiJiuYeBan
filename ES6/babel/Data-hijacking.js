a94a68ef9153ee5565b12e7c6e6e50a21810b878
Adam <caiyijias@outlook.com>
2019-08-02 01:15:03 +0800

    数组数据劫持原理

--- ES6/babel/Data-hijacking.js
+++ ES6/babel/Data-hijacking.js
@@ -2,29 +2,63 @@ var oDiv = document.getElementById('show');
 var oInput = document.getElementById('demo');
 
 var oData = {
-    value: 'duyi'
+    valueObj: {
+        value: 'duyi'
+    },
+    sex: 'm'
 };
 
 oInput.oninput = function () {
     oData.value = this.value;
 }
 
-function upDate () {
-    oDiv.innerText = oData.value;
+function upDate() {
+    oDiv.innerText = oData.valueObj.value;
 }
 
-upDate();
+// upDate();
 
-function Observer (data) {
+function Observer(data) {
     if (!data || typeof data != 'object') {
         return data;
     }
-    Object.keys(data).forEach(function(key) {
+    Object.keys(data).forEach(function (key) {
         definedReactive(data, key, data[key]);
     })
 }
-function definedReactive (data, key, val) {
 
+function definedReactive(data, key, val) {
+    Observer(val);
+    Object.defineProperty(data, key, {
+        get() {
+            return val;
+        },
+        set(newValue) {
+            if (newValue == val) return;
+            val = newValue;
+            upDate();
+
+        }
+    })
 }
 
 Observer(oData);
+
+// console.log(oData.value);
+// oData.valueObj.value = 'haha'
+
+let arr = [];
+let {push} = Array.prototype;
+
+function update() {
+    console.log('update');
+}
+
+Object.defineProperty(Array.prototype, 'push', {
+    value: (() => ((...arg) => {
+        push.apply(arr, arg);
+        update();
+    }))()
+});
+
+arr.push(1,2)
