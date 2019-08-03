c0ce6657d021a1840c222d1d09289cdf60c3c90c
Adam <caiyijias@outlook.com>
2019-08-03 21:19:52 +0800

    完成ES5实现Promise异步执行

--- ES6/babel/PromiseMy.js
+++ ES6/babel/PromiseMy.js
@@ -3,16 +3,24 @@ function MyPromise(executor) {
     self.status = 'pending';
     self.resolveValue = null;
     self.rejectReason = null;
+    self.ResolveCallBackList = [];
+    self.RejectCallBackList = [];
     function resolve(value) {
         if(self.status == 'pending' ){
             self.status = 'Fulfilled';
             self.resolveValue = value;
+            self.ResolveCallBackList.forEach(function(ele) {
+                ele()
+            })
         }
     }
     function reject(reason) {
         if(self.status === 'pending') {
             self.status = 'Rejected';
             self.rejectReason = reason;
+            self.RejectCallBackList.forEach(function(ele) {
+                ele()
+            })
         }
     }
 
@@ -31,6 +39,15 @@ MyPromise.prototype.then = function(onFulfilled, onRejected) {
     }
     if(self.status === 'Rejected') {
         onRejected(self.rejectReason);
+    }
+
+    if(self.status === 'pending'){
+        self.ResolveCallBackList.push(function(){
+            onFulfilled(self.resolveValue);
+        });
+        self.RejectCallBackList.push(function() {
+            onRejected(self.rejectReason);
+        })
     }
 }
 
