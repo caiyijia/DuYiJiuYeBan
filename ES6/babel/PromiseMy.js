ddaddc29b2b9df4ad1fa1d4aae2022bd12b3a627
Adam <caiyijias@outlook.com>
2019-08-03 21:45:46 +0800

    完成ES5实现Promise链式调用

--- ES6/babel/PromiseMy.js
+++ ES6/babel/PromiseMy.js
@@ -5,49 +5,58 @@ function MyPromise(executor) {
     self.rejectReason = null;
     self.ResolveCallBackList = [];
     self.RejectCallBackList = [];
+
     function resolve(value) {
-        if(self.status == 'pending' ){
+        if (self.status == 'pending') {
             self.status = 'Fulfilled';
             self.resolveValue = value;
-            self.ResolveCallBackList.forEach(function(ele) {
+            self.ResolveCallBackList.forEach(function (ele) {
                 ele()
             })
         }
     }
+
     function reject(reason) {
-        if(self.status === 'pending') {
+        if (self.status === 'pending') {
             self.status = 'Rejected';
             self.rejectReason = reason;
-            self.RejectCallBackList.forEach(function(ele) {
+            self.RejectCallBackList.forEach(function (ele) {
                 ele()
             })
         }
     }
 
     // 即使有错误也要继续执行
-    try{
-        executor(resolve,reject)
-    }catch(e) {
+    try {
+        executor(resolve, reject)
+    } catch (e) {
         reject(e)
     }
 }
 
-MyPromise.prototype.then = function(onFulfilled, onRejected) {
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
     var self = this;
-    if(self.status == 'Fulfilled') {
-        onFulfilled(self.resolveValue);
-    }
-    if(self.status === 'Rejected') {
-        onRejected(self.rejectReason);
-    }
+    var nextPromise = new MyPromise(function (res, rej) {
+        if (self.status == 'Fulfilled') {
+            var nextResolveValue = onFulfilled(self.resolveValue);
+            res(nextResolveValue)
+        }
+        if (self.status === 'Rejected') {
+            var nextRejectValue = onRejected(self.rejectReason);
+            rej(nextRejectValue);
+        }
 
-    if(self.status === 'pending'){
-        self.ResolveCallBackList.push(function(){
-            onFulfilled(self.resolveValue);
-        });
-        self.RejectCallBackList.push(function() {
-            onRejected(self.rejectReason);
-        })
-    }
+        if (self.status === 'pending') {
+            self.ResolveCallBackList.push(function () {
+                var nextResolveValue = onFulfilled(self.resolveValue);
+                res(nextResolveValue)
+            });
+            self.RejectCallBackList.push(function () {
+                var nextRejectValue = onRejected(self.rejectReason);
+                rej(nextRejectValue);
+            })
+        }
+    })
+
+    return nextPromise;
 }
-
