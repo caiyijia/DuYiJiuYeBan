e7dccc38967464e3be02f2b705efe17a63ab270f
Adam <caiyijias@outlook.com>
2019-08-03 20:26:07 +0800

    同步并发的异步操作结果

--- ES6/babel/Promise.js
+++ ES6/babel/Promise.js
@@ -1,19 +1,27 @@
 let fs = require('fs');
 
+let oStudent = {};
+
 function readFile(path) {
+    if (!path || path == 'undefined') return;
+    console.log(path)
     return new Promise((res, rej) => {
-        fs.readFile(path, 'utf-8', (err,data) => {
-            if(data) {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (data) {
                 res(data)
             }
         })
     })
 }
 
-readFile('./data/number.txt').then((data) => {
-    return readFile(data);
-}).then((data) => {
-    return readFile(data);
-}).then((data) => {
-    console.log(data)
+// promise对象
+Promise.all([readFile('./data/number.txt'), readFile('./data/name.txt'), readFile('./data/score.txt')]).then((val) => {
+    console.log(val)
 })
+// readFile('./data/number.txt').then((data) => {
+//     return readFile(data);
+// }).then((data) => {
+//     return readFile(data);
+// }).then((data) => {
+//     console.log(data)
+// })
